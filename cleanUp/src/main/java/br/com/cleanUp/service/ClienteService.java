50038fb997c2b26359711e88d1d22c69397f5393
Marco <Marco@Marco-PC>
2014-09-05 21:12:52 -0300

    Criação da Classe Basica Cliente

--- cleanUp/src/main/java/br/com/cleanUp/service/ClienteService.java
+++ cleanUp/src/main/java/br/com/cleanUp/service/ClienteService.java
@@ -1,21 +1,34 @@
 package br.com.cleanUp.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import br.com.cleanUp.model.Pessoa;
-import br.com.cleanUp.repository.PessoaRepository;
+import br.com.cleanUp.model.Cliente;
+import br.com.cleanUp.repository.ClienteRepository;
 
 @Service
 @Transactional
 public class ClienteService {
 	
 	@Autowired
-    private PessoaRepository pessoaRepository;
+    private ClienteRepository clienteRepository;
 
-	public void save(Pessoa p){
-		pessoaRepository.save(p);
+	public void salvarCliente(Cliente cli){
+		clienteRepository.save(cli);
 	}
 	
+	public void editarCliente(Cliente cli){
+		clienteRepository.delete(clienteRepository.findOne(cli.getCodigo()));
+		clienteRepository.save(cli);
+	}
+	
+	public void removerCliente(Cliente cli){
+		clienteRepository.delete(cli);
+	}
+	
+	public List<Cliente> listCliente(){
+		return (List<Cliente>) clienteRepository.findAll();
+	}
 }
