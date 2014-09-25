05ae3e931be8ca123a6036e9d5b16cf2ebce141f
filipeleao <filipeleaosoares@gmail.com>
2014-09-25 17:36:29 -0300

    - criação do controller, repository, service de Serviço.

---
+++ cleanUp/src/main/java/br/com/cleanUp/repository/ServicoRepository.java
@@ -0,0 +1,13 @@
+package br.com.cleanUp.repository;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+
+//public interface ServicoRepository extends CrudRepository<Servico, Integer> {
+////	@Query(value = "SELECT s FROM servico AS u WHERE diarista = ? AND  data > CURRENT_DATE()")
+////	List<Servico> findByDiarista(Integer codigo, Date data);
+//}
---
+++ cleanUp/src/main/java/br/com/cleanUp/service/ServicoService.java
@@ -0,0 +1,24 @@
+package br.com.cleanUp.service;
+
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import br.com.cleanUp.repository.ServicoRepository;
+//
+//@Service
+//@Transactional
+//public class ServicoService {
+//	
+//	@Autowired
+//    private ServicoRepository servicoRepository;
+//
+//	public void save(Servico s){
+//		servicoRepository.save(s);
+//	}
+//	
+//	public void getDiaLivre(Servico s){
+//		servicoRepository.findByDiarista(s,s);
+//	}
+//	
+//}
