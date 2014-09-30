853a3942f80d7e10ff8e3a9b81e6fc76d5458b82
Jeyson <Jeyson@Jeyson-PC>
2014-09-30 09:35:45 -0300

    Aleração da Classe Endereco

--- cleanUp/src/main/java/br/com/cleanUp/model/Endereco.java
+++ cleanUp/src/main/java/br/com/cleanUp/model/Endereco.java
@@ -1,13 +1,27 @@
 package br.com.cleanUp.model;
 
 import javax.persistence.Column;
-import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-@Embeddable
+@Entity
 public class Endereco {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name = "CODIGO")
+	private Integer codigo;
 	
 	@Column(name = "ENDERECO", length = 150, nullable = false)
 	private String endereco;
+	
+	@Column(name = "LATITUDE")
+	private long lat;
+	
+	@Column(name = "LOGITUDE")
+	private long log;
 	
 	public Endereco(){
 		
@@ -24,5 +38,28 @@ public class Endereco {
 	public void setEndereco(String endereco) {
 		this.endereco = endereco;
 	}
-	
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public long getLat() {
+		return lat;
+	}
+
+	public void setLat(long lat) {
+		this.lat = lat;
+	}
+
+	public long getLog() {
+		return log;
+	}
+
+	public void setLog(long log) {
+		this.log = log;
+	}
 }
