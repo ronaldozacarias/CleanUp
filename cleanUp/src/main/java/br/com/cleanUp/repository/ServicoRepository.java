876ce9108a060c98d81e927d539c9271412a7215
Jeyson <Jeyson@Jeyson-PC>
2014-10-02 23:10:32 -0300

    Implementei a interface ServicoRepository

--- cleanUp/src/main/java/br/com/cleanUp/repository/ServicoRepository.java
+++ cleanUp/src/main/java/br/com/cleanUp/repository/ServicoRepository.java
@@ -1,13 +1,8 @@
 package br.com.cleanUp.repository;
 
-import java.util.Date;
-import java.util.List;
-
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import br.com.cleanUp.model.Servico;
 
 
-//public interface ServicoRepository extends CrudRepository<Servico, Integer> {
-////	@Query(value = "SELECT s FROM servico AS u WHERE diarista = ? AND  data > CURRENT_DATE()")
-////	List<Servico> findByDiarista(Integer codigo, Date data);
-//}
+public interface ServicoRepository extends CrudRepository<Servico, Integer> {
+}
