e76d1c33c4b3713c5f7b5d73eb448a0da306a1dc
Jeyson <Jeyson@Jeyson-PC>
2014-10-02 23:06:46 -0300

    Implementei a classe servicoService

--- cleanUp/src/main/java/br/com/cleanUp/service/ServicoService.java
+++ cleanUp/src/main/java/br/com/cleanUp/service/ServicoService.java
@@ -1,24 +1,43 @@
 package br.com.cleanUp.service;
 
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import br.com.cleanUp.repository.ServicoRepository;
-//
-//@Service
-//@Transactional
-//public class ServicoService {
-//	
-//	@Autowired
-//    private ServicoRepository servicoRepository;
-//
-//	public void save(Servico s){
-//		servicoRepository.save(s);
-//	}
-//	
-//	public void getDiaLivre(Servico s){
-//		servicoRepository.findByDiarista(s,s);
-//	}
-//	
-//}
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.cleanUp.controller.DiaristaController;
+import br.com.cleanUp.exception.NegocioException;
+import br.com.cleanUp.model.Servico;
+import br.com.cleanUp.repository.ServicoRepository;
+
+@Service
+@Transactional
+public class ServicoService {
+	
+	@Autowired
+	private ServicoRepository servicoRepository;
+
+	public void save(ArrayList<Servico> s) throws NegocioException{
+		DiaristaController dc = new DiaristaController();
+		try {
+			for (int i = 0; i < s.size(); i++) {
+				servicoRepository.save(s.get(i));
+			}
+			dc.solicitacaoDeServico(s);
+		} catch (Exception e) {
+			throw new NegocioException("Erro ao Salvar Servico");
+		}
+	}
+	
+	public void edit(ArrayList<Servico> s)throws NegocioException{
+		try {
+			for (int i = 0; i < s.size(); i++) {
+				servicoRepository.delete(s.get(i).getCodServico());
+				servicoRepository.save(s.get(i));
+			}
+		} catch (Exception e) {
+			throw new NegocioException("Erro ao Salvar Servico");
+		}
+	}
+}
