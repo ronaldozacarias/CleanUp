40641f8d1b5103f38545e9ff4957dbabd9482fc9
wellington <wellington@TOMWELL>
2014-10-29 17:40:47 -0300

    AceitarServiçoVO

---
+++ cleanUp/src/main/java/br/com/cleanUp/vo/AceitarServicoVO.java
@@ -0,0 +1,21 @@
+package br.com.cleanUp.vo;
+
+import java.util.ArrayList;
+
+import br.com.cleanUp.model.Servico;
+
+public class AceitarServicoVO {
+	
+	ArrayList<Servico> servicosVO;
+
+	public ArrayList<Servico> getServicosVO() {
+		return servicosVO;
+	}
+
+	public void setServicosVO(ArrayList<Servico> servicosVO) {
+		this.servicosVO = servicosVO;
+	}
+	
+	
+
+}
